0b3e89779a9d34ba1b566285ac48ecafaa0a01a1
Sashankr <sashankr97@gmail.com>
2023-03-14 14:02:27 +0530

    added page to show post details

--- src/components/PostCard/index.jsx
+++ src/components/PostCard/index.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { useRouter } from "next/router";
 import styles from "./styles.module.css";
 
 const PostCard = ({ id, title, userId, content }) => {
+  const router = useRouter();
+
   return (
-    <div className={`${styles["post-card"]} ${styles["border-gradient"]}`}>
+    <div
+      className={`${styles["post-card"]}`}
+      onClick={() => {
+        router.push(`/posts/${id}`);
+      }}
+    >
       <h2>{title}</h2>
       <p>{content}</p>
     </div>
--- src/pages/posts/[id].jsx
+++ src/pages/posts/[id].jsx
@@ -1,11 +1,31 @@
 import React from "react";
 import { useRouter } from "next/router";
+import { useGetPostQuery } from "@/features/api/apiSlice";
 
 const PostDetails = (props) => {
   const router = useRouter();
   const { id } = router.query;
 
-  return <div>PostDetails {id} </div>;
+  const {
+    data: post,
+    isLoading: isPostLoading,
+    isSuccess: isPostSuccess,
+    isError: isPostError,
+    error: postError,
+  } = useGetPostQuery(id);
+
+  if (isPostLoading) {
+    return <h2>Loading...</h2>;
+  } else if (isPostError) {
+    return <h2>An error occured : {postError.message}</h2>;
+  }
+
+  return (
+    <div>
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+    </div>
+  );
 };
 
 export default PostDetails;
