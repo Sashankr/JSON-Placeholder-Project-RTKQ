574f461eff4f799f6d7d717134e5461aa750d878
Sashankr <sashankr97@gmail.com>
2023-03-15 15:57:36 +0530

    created new post create page

---
+++ src/pages/createPost.js
@@ -0,0 +1,33 @@
+import React from "react";
+import globalStyle from "@/styles/Home.module.css";
+import styles from "@/styles/createPost.module.css";
+
+const createPost = () => {
+  return (
+    <div className={globalStyle.container}>
+      <div className={styles["create-post"]}>
+        <h1>Create New Post</h1>
+        <p>Fill the following fields to create a new post</p>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+          }}
+        >
+          <input type="text" placeholder="Title" required />
+          <textarea
+            name=""
+            id=""
+            cols="30"
+            rows="10"
+            placeholder="Body"
+            required
+          ></textarea>
+          <input type="text" placeholder="UserId" required />
+          <button type="submit">Create Post</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default createPost;
